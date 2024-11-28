927d25d969bfeafdcfd25364395fbef7cf88bdf6
Candela <candelamarzaroli@gmail.com>
2024-11-27 23:48:16 -0300

    🐱‍👤

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "./Home/Home";
 import Game from "./Game/Game"
-import Paint from "./Components/Paint";
-import Winning from "./Components/Winning";
 
 
 
@@ -15,8 +13,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/play/:difficulty/:players" element={<Game />} />
-        <Route path="/paint" element={<Paint />} />
-        <Route path="/win" element={<Winning winner="match"/>} />
       </Routes>
     </BrowserRouter>
   );
