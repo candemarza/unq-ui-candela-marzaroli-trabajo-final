f637cb691ebdd7a64b7ef0f79decddb9c08cc0c0
Candela <candelamarzaroli@gmail.com>
2024-12-04 23:17:10 -0300

    card un poquitito reactive

--- src/Game/Game.jsx
+++ src/Game/Game.jsx
@@ -23,7 +23,7 @@ const Game = ({ numOfPlayers, onBackToHome, difficulty }) => {
 
   useEffect(() => {
     //set de inicio de juego
-    const shuffledList = shuffle(list);
+    const shuffledList = list;
     setShuffledCards(
       shuffledList.map((color, i) => ({
         index: i,
