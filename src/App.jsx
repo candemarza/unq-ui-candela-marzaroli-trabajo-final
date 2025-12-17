3314a1cecc62f830b095cde9bc28d3b7e6742754
Candela <candelamarzaroli@gmail.com>
2025-12-16 23:26:21 -0300

    fix routing y diccionario

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
 import Home from "./Home/Home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Home/>
   );
 }
 
---
+++ src/colorHexdictionary.js
@@ -0,0 +1,39 @@
+const colorHexDictionary = {
+    "Red": "#FF0000",
+    "Orange": "#FFA500",
+    "Yellow": "#FFFF00",
+    "Green": "#008000",
+    "Cyan": "#00FFFF",
+    "Blue": "#0000FF",
+    "Purple": "#800080",
+    "Magenta": "#FF00FF",
+    //easy ^^
+    "Slate Gray": "#708090",
+    "Fire Brick": "#B22222",
+    "Khaki": "#F0E68C",
+    "Yellow Green": "#9ACD32",
+    "Dark Cyan": "#008B8B",
+    "Ghost White": "#F8F8FF",
+    "Rebecca Purple": "#663399",
+    "Orchid": "#DA70D6",
+    "Medium Violet Red": "#C71585",
+    "Midnight Blue": "#191970",
+    //medium ^^
+    "Tomato": "#FF6347",
+    "Spring Green": "#00FF7F",
+    "Dark Turquoise": "#00CED1",
+    "Medium Purple": "#9370DB",
+    "Royal Blue": "#4169E1",
+    "Saddle Brown": "#8B4513",
+    "Indigo": "#4B0082",
+    "Sea Green": "#2E8B57",
+    "Pink": "#FFBEC2",
+    "Golden Rod": "#DAA520",
+    "Thistle": "#D8BFD8",
+    "Light Coral": "#F08080",
+    "Olive Drab": "#6B8E23",
+    "Chocolate": "#D2691E"
+    //hard ^^
+  };
+  
+export default colorHexDictionary;
